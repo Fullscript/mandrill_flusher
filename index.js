06227538d767ca1bade2ccfbd60a77864b241474
PatrickGVice <patrickgvice@gmail.com>
2014-10-08 17:18:05 -0400

    removed usless console.logs, and improved code style

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var password = process.argv[3];
 var mandrill = new Nightmare()
   .use( mandrill.login(username, password) )
   .use( mandrill.getWebHooks() )
-  //mandrill.webhooks is an array with all the webhooks links
   .use( mandrill.collectWebHook() )
   .use( mandrill.giveUpHooks() )
   .run()
